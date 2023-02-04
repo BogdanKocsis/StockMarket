6a98f3459164073e029f84481d57537791242abd
bogdan.kocsis <bogdan.kocsis@fortech.ro>
2023-02-04 23:01:06 +0200

    Added work manager for notifications.

--- app/src/main/java/com/pdmpa/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/pdmpa/stockmarketapp/data/remote/StockApi.kt
@@ -1,10 +1,8 @@
 package com.pdmpa.stockmarketapp.data.remote
 
 import com.pdmpa.stockmarketapp.data.remote.dto.CompanyInfoDto
-import com.pdmpa.stockmarketapp.domain.model.NewsDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StockApi {
@@ -25,21 +23,6 @@ interface StockApi {
         @Query("symbol") symbol: String,
         @Query("apikey") apiKey: String = "WBYPBRI3B2MT7Z79"
     ): CompanyInfoDto
-
-    @GET("v1/news/{filter}")
-    suspend fun getNews(
-        @Path("filter") filter: String,
-        /**
-         * available filters
-         * 1. handpicked
-         * 2. trending
-         * 3. latest
-         * 4. bullish
-         * 5. bearish
-         */
-        @Query("limit") limit: Int = 20,
-        @Query("skip") skip: Int = 0
-    ): NewsDto
 
     companion object {
         const val BASE_URL = "https://alphavantage.co"
